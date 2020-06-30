4d692de1015525182e4e2ab30be585348408487b
Ladislav Zezula <ladislav.zezula@avast.com>
2020-06-30 22:55:29 +0200

    ImageLoader: Tests OK

--- src/fileformat/file_format/pe/pe_format.cpp
+++ src/fileformat/file_format/pe/pe_format.cpp
@@ -402,6 +402,8 @@ const std::unordered_set<std::string> usualPackerSections
 	"pec4",
 	"pec5",
 	"pec6",
+	"gu_idata",             // Created by retdec-unpacker
+	"gu_rsrc"               // Created by retdec-unpacker
 };
 
 const std::map<std::string, std::size_t> usualSectionCharacteristics
@@ -3625,7 +3627,9 @@ void PeFormat::scanForSectionAnomalies(unsigned anamaliesLimit)
 {
 	std::size_t nSecs = getDeclaredNumberOfSections();
 
+	unsigned long long imageBase;
 	unsigned long long epAddr;
+
 	if (getEpAddress(epAddr))
 	{
 		auto *epSec = dynamic_cast<const PeCoffSection*>(getEpSection());
@@ -3651,9 +3655,7 @@ void PeFormat::scanForSectionAnomalies(unsigned anamaliesLimit)
 		else
 		{
 			// scan EP outside mapped sections
-			anomalies.emplace_back(
-				"EpOutsideSections", "Entry point is outside of mapped sections"
-			);
+			anomalies.emplace_back("EpOutsideSections", "Entry point is outside of mapped sections");
 		}
 	}
 
@@ -3757,20 +3759,24 @@ void PeFormat::scanForSectionAnomalies(unsigned anamaliesLimit)
 				continue;
 			}
 
-			// scan for overlapping sections
-			auto secStart = sec->getOffset();
-			auto secEnd = secStart + sec->getSizeInFile();
-			const auto cmpName = cmpSec->getName();
-			auto cmpSecStart = cmpSec->getOffset();
-			auto cmpSecEnd = cmpSecStart + cmpSec->getSizeInFile();
-			if ((secStart <= cmpSecStart && cmpSecStart < secEnd) ||
-				(cmpSecStart <= secStart && secStart < cmpSecEnd))
+			// scan for overlapping sections.
+			// DO NOT check if the previous section has zero size.
+			if(sec->getSizeInFile() != 0)
 			{
-				const std::string cmpMsgName = cmpName.empty() ? std::to_string(cmpSec->getIndex()) : cmpName;
-				anomalies.emplace_back(
+				auto secStart = sec->getOffset();
+				auto secEnd = secStart + sec->getSizeInFile();
+				const auto cmpName = cmpSec->getName();
+				auto cmpSecStart = cmpSec->getOffset();
+				auto cmpSecEnd = cmpSecStart + cmpSec->getSizeInFile();
+				if((secStart <= cmpSecStart && cmpSecStart < secEnd) ||
+					(cmpSecStart <= secStart && secStart < cmpSecEnd))
+				{
+					const std::string cmpMsgName = cmpName.empty() ? std::to_string(cmpSec->getIndex()) : cmpName;
+					anomalies.emplace_back(
 						"OverlappingSections",
 						"Sections " + pmsgName + " and " + replaceNonprintableChars(cmpMsgName) + " overlap"
-				);
+					);
+				}
 			}
 		}
 	}
